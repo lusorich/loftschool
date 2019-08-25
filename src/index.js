a1eacb67bd74fe1a9ef014d941cfa4d15f518391
Lusorich <dreamonautre@gmail.com>
2019-08-25 21:55:11 +0500

    Tasks 1,2,4,5

--- src/index.js
+++ src/index.js
@@ -7,8 +7,22 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
-}
+    if (!Array.isArray(array)) {
+        return 'Нужно передать массив'
+    }
 
+    if (array.length === 0) {
+        return 'Вы передали пустой массив';
+    }
+
+    if (typeof(fn) !== 'function') {
+        return 'Нужно передать функцию'
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        fn(array[i], i ,array);
+    }
+}
 /*
  Задание 2:
 
@@ -16,6 +30,25 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
+	if (!Array.isArray(array)) {
+        return 'Нужно передать массив'
+    }
+    
+    if (array.length === 0) {
+        return 'Вы передали пустой массив';
+    }
+
+    if (typeof(fn) !== 'function') {
+      return 'Нужно передать функцию'
+    }
+
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(fn(array[i], i ,array));
+    }
+
+    return newArray;
 }
 
 /*
@@ -25,7 +58,46 @@ function map(array, fn) {
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
 function reduce(array, fn, initial) {
+	if (!Array.isArray(array)) {
+        return 'Нужно передать массив'
+    }
+    
+    if (array.length === 0) {
+        return 'Вы передали пустой массив';
+    }
+
+    if (typeof(fn) !== 'function') {
+      return 'Нужно передать функцию'
+    }
+
+    let acc = initial === undefined ? 0 : initial;
+    let prev;
+    let current;
+
+    for (let i = 0; i < array.length; i++) {
+        current = array[i];
+        if (i === 0) {
+      	    prev = initial ? initial : array[0];
+        } 
+      
+        if (i === 1) {
+            prev = initial ? initial + array[0] : array[0];
+        }
+
+        if (i > 1) {
+             prev = acc;
+        }
+
+        console.log(`prev is ${prev}`);
+        console.log(`current is ${current}`);
+
+
+        acc = fn(prev, current, i, array);
+    }  
+    
+    return acc;
 }
+
 
 /*
  Задание 4:
@@ -36,6 +108,13 @@ function reduce(array, fn, initial) {
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
+  let array = [];
+
+  for (var key in obj) {
+    array.push(key.toUpperCase());
+  }
+
+  return array;
 }
 
 /*
@@ -44,7 +123,44 @@ function upperProps(obj) {
  Напишите аналог встроенного метода slice для работы с массивами
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
-function slice(array, from, to) {
+
+function slice(array, from = 0, to = array.length) {
+  let newArray = [];
+
+  if (from < 0 && Math.abs(from) <= array.length) {
+    from = array.length + from;
+  }
+
+  if (from < 0 && Math.abs(from) > array.length) {
+  	from = 0;
+  }
+
+  if (to < 0) {
+    to = array.length + to;
+  }
+
+  if ( typeof(from) !== 'number') {
+    from = 0;
+  }
+
+  if (from > array.length) {
+    return [];
+  }
+
+  if (to > array.length) {
+  	to = array.length;
+  }
+
+  if (to < from || to === from) {
+    return [];
+  }
+
+  
+  for (let i = from; i < to; i++) {
+    newArray.push(array[i]);
+  }
+
+  return newArray;
 }
 
 /*
@@ -53,8 +169,8 @@ function slice(array, from, to) {
  Функция принимает объект и должна вернуть Proxy для этого объекта
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
-function createProxy(obj) {
-}
+//function createProxy(obj) {
+//}
 
 export {
     forEach,
