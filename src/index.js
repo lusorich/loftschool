803d8ebfc8e7fb3f09fba6c7bdf8fd9047f4138a
Lusorich <dreamonautre@gmail.com>
2019-09-03 22:27:19 +0500

    Tasks 1-6 complete

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
+
     target.addEventListener(eventName, fn);
 }
 
@@ -21,6 +22,8 @@ function addListener(eventName, target, fn) {
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
+
+    target.removeEventListener(eventName, fn);
 }
 
 /*
@@ -32,6 +35,10 @@ function removeListener(eventName, target, fn) {
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
+
+    target.addEventListener(eventName, (eventName) => {
+        eventName.preventDefault();
+    });
 }
 
 /*
@@ -43,6 +50,10 @@ function skipDefault(eventName, target) {
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
 function emulateClick(target) {
+
+    let event = new Event('click');
+
+    target.dispatchEvent(event);
 }
 
 /*
@@ -55,6 +66,12 @@ function emulateClick(target) {
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
+
+    for (let i = 0; i < target.childNodes.length; i++) {
+        if (target.childNodes[i].nodeName === 'BUTTON') {
+            target.childNodes[i].addEventListener('click', fn);
+        }
+    }
 }
 
 /*
@@ -67,6 +84,11 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
+
+    target.addEventListener('click', function bar() {
+        fn();
+        target.removeEventListener('click', bar, false);
+    });
 }
 
 export {
