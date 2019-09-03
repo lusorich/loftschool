0d17e796d12aad22ef0e442e1fbee62278a87324
lusorich <dreamonautre@gmail.com>
2019-09-03 17:02:48 +0500

    tasks 1 complete

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
+    target.addEventListener(eventName, fn);
 }
 
 /*
